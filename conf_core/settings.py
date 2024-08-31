5e8b3962f3ec2a394a100decedf4e36ecc40c15d
Rohit Kumar <rohit.hrp88@gmail.com>
2024-08-31 07:52:43 +0530

    initial project layout creates

--- conf_core/settings.py
+++ conf_core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'common',
+    'pages',
+    'authentication'
 ]
 
 MIDDLEWARE = [
@@ -120,7 +122,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# URL to use when referring to static files
+STATIC_URL = '/static/'
+
+# The directory where Django will collect static files for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional locations of static files (CSS, JS, Images)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# URL to use for serving media files
+MEDIA_URL = '/media/'
+
+# Directory to store uploaded media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
